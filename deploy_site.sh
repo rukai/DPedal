9cfbb99e76e2bbc6386372297bcf513fff080dd1
Lucas Kent <rubickent@gmail.com>
2025-08-22 21:52:02 +1000

    Background numbers

---
+++ deploy_site.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -u
+
+cd site
+
+OUTPUT_BUCKET_NAME=dpedal.com
+
+# just deploy it all with no cache and no compression, we can come back to this later
+aws s3 sync . s3://$OUTPUT_BUCKET_NAME --cache-control no-cache
