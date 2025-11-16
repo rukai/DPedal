efbb90137a0d766f885572ec595775cacb4904df
Lucas Kent <rubickent@gmail.com>
2025-11-16 18:11:57 +1100

    add step file to kicad export script

--- export_pcb.sh
+++ export_pcb.sh
@@ -4,13 +4,17 @@ set -e
 
 OUT=target/kicad/dpedal_gerber_files
 ZIP=dpedal_gerber_files.zip
+PCB=pcb/dpedal_pcb.kicad_pcb
 
 rm -f $ZIP
 
 mkdir -p $OUT
-kicad-cli pcb export drill --output $OUT pcb/dpedal_pcb.kicad_pcb
-kicad-cli pcb export gerbers --output $OUT pcb/dpedal_pcb.kicad_pcb
+kicad-cli pcb export drill --output $OUT $PCB
+kicad-cli pcb export gerbers --output $OUT $PCB
 cd target/kicad
 zip $ZIP dpedal_gerber_files/*
 mv $ZIP ../..
+cd -
+
+kicad-cli pcb export step --subst-models --output dpedal_pcb.step $PCB
 
